1586cd2754f8eafd35e1583ec99d818d4053b494
罗文杰 <wenjie_luo@hydf.cn>
2020-07-02 20:25:23 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+var paths = require('./webpack.paths');
+var loaders = require('./webpack.loaders');
+
+
+module.exports = {
+	entry: {
+		main: paths.app
+	},
+	resolve: {
+		root: paths.app,
+		extensions: ['', '.js', '.ts']
+	},
+	output: {
+		path: paths.build,
+		filename: 'bundle.js',
+		publicPath: 'build/'
+	},
+	devtool: 'eval-source-map',
+	devServer: {
+		historyApiFallback: true,
+		hot: true,
+		inline: true,
+		progress: true
+	},
+	module: {
+		loaders: loaders
+	},
+	plugins: [
+		new webpack.NoErrorsPlugin(),
+		new webpack.DefinePlugin({
+			BUILD_MODE: JSON.stringify('development')
+		})
+	]
+};
